2283000b7ec927e2eccd3bf9e5c4400863402d8f
NormalSeed <dlrjsghk11@naver.com>
2025-04-01 14:30:55 +0900

    [Feat] Write Game code, Create Scene Class
    게임 클래스에 기본틀 구현,
    Scene 추상클래스 구현

--- TextRPG/TextRPG/Game.cs
+++ TextRPG/TextRPG/Game.cs
@@ -8,6 +8,27 @@ namespace TextRPG
 {
     public static class Game
     {
+        // 게임에 필요한 정보
+        private static bool gameOver;
 
+        // 게임에 필요한 기능
+        // 시작, 실행, 끝
+        public static void Start()
+        {
+
+        }
+
+        public static void Run()
+        {
+            while (gameOver == false)
+            {
+
+            }
+        }
+
+        public static void End()
+        {
+
+        }
     }
 }
---
+++ TextRPG/TextRPG/Scene/Scene.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.Scene
+{
+    public abstract class Scene
+    {
+        protected ConsoleKey input;
+        // <summary>
+        // 씬의 상황 작성
+        // </summary>
+        public abstract void Render();
+
+        // <summary>
+        // 씬의 선택지 제시
+        // </summary>
+        public abstract void Choice();
+
+        // <summary>
+        // 선택지 입력
+        // </summary>
+        public void Input(ConsoleKey input)
+        {
+            input = Console.ReadKey(true).Key;
+        }
+
+        // <summary>
+        // 선택지에 따른 결과 출력
+        // </summary>
+        public abstract void Result();
+
+        // <summary>
+        // 다음 씬으로 가기 전 대기
+        // </summary>
+        public abstract void Wait();
+
+        // <summary>
+        // 다음 씬으로 전환
+        // </summary>
+        public abstract void Next();
+    }
+}
