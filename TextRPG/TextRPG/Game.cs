16c65efb301b091acdcbeb86f6e3e69e1c79f116
NormalSeed <dlrjsghk11@naver.com>
2025-04-01 14:53:21 +0900

    [Feat] Create TitleScene
    타이틀 화면 구현

--- TextRPG/TextRPG/Game.cs
+++ TextRPG/TextRPG/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG.Scenes;
 
 namespace TextRPG
 {
@@ -10,19 +11,30 @@ namespace TextRPG
     {
         // 게임에 필요한 정보
         private static bool gameOver;
+        private static Dictionary<string, Scene> sceneDic;
+        private static Scene curScene;
 
         // 게임에 필요한 기능
         // 시작, 실행, 끝
         public static void Start()
         {
-
+            sceneDic = new Dictionary<string, Scene>();
+            sceneDic.Add("Title", new TitleScene());
+            curScene = sceneDic["Title"];
         }
 
         public static void Run()
         {
             while (gameOver == false)
             {
+                Console.Clear();
 
+                curScene.Render();
+                curScene.Choice();
+                curScene.Input();
+                curScene.Result();
+                curScene.Wait();
+                curScene.Next();
             }
         }
 
---
+++ TextRPG/TextRPG/Scenes/TitleScene.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.Scenes
+{
+    public class TitleScene : Scene
+    {
+        public override void Render()
+        {
+            Console.WriteLine("**********************");
+            Console.WriteLine("*     텍스트 RPG     *");
+            Console.WriteLine("**********************");
+            Console.WriteLine();
+        }
+
+        public override void Choice()
+        {
+            Console.WriteLine("1. 게임시작");
+            Console.WriteLine("2. 불러오기(미구현)");
+            Console.WriteLine("3. 게임종료");
+        }
+
+        public override void Result()
+        {
+
+        }
+
+        public override void Wait()
+        {
+            
+        }
+
+        public override void Next()
+        {
+            switch (input)
+            {
+                case ConsoleKey.D1:
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+}
